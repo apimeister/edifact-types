2a9fb03cd8e9e9c49cab94ed56a60b24df892c07
schoki040 <a.kunde@outlook.com>
2023-11-06 15:40:02 +0100

    Interchange info

--- src/d00b/mod.rs
+++ src/d00b/mod.rs
@@ -23,6 +23,35 @@ pub use message::iftsta::*;
 #[cfg(test)]
 mod test_segment;
 
+
+/// from: [official info](https://unece.org/fileadmin/DAM/trade/edifact/untdid/d422_s.htm)
+/// 6.1 Interchange structure
+/// 
+/// The Service String Advice, UNA, and the service segments UNB
+/// to UNZ shall appear in the below stated order in an
+/// interchange. There may be several functional groups or
+/// messages within an interchange and several messages in a
+/// functional group. A message consists of segments. The
+/// structures for segments and for data elements therein are
+/// shown in 6.2 and 6.3. The contents of the service segments
+/// are shown annex B. See also figure 1.
+/// 
+/// An interchange consists of:
+/// 
+/// x | x | x | x | Name | Abbr. | Req.
+/// --- | --- | --- | --- | --- | --- | ---
+/// o | o | o | o | Service String Advice | UNA | Conditional
+/// _ | _ | _ | _ | Interchange Header | UNB | Mandatory
+/// \| | _ | _ | _ | Functional Group Header | UNG | Conditional
+/// \| | \| | _ | _ | Message Header | UNH | Mandatory
+/// \| | \| | \| |   | User Data Segments |   | As required
+/// \| | \| | \| | _ | Message Trailer | UNT | Mandatory
+/// \| | \| | _ | _ | Functional Group Trailer | UNE | Conditional
+/// \| | _ | _ | _ | Interchange Trailer | UNZ | Mandatory
+/// 
+/// In addition to the above service segments, the service
+/// segment UNS can, when required, be used to divide a message
+/// into sections. See annex B (NOT IMPLEMENTED).
 #[derive(Serialize, Deserialize, Clone, Default, Debug, PartialEq, Eq, DisplayEdifact)]
 pub struct Interchange<T>
 where
